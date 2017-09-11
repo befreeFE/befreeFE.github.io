bbff2c8a42e6466258d28ee670339e81ac0178b8
le <445220716@qq.com>
2017-09-12 01:27:50 +0800

    init

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,143 @@
+var path = require('path')
+var utils = require('./utils')
+var config = require('../config')
+var vueLoaderConfig = require('./vue-loader.conf')
+var md = require('markdown-it')()
+var slugify = require('transliteration').slugify
+var striptags = require('./strip-tags')
+function convert (str) {
+  str = str.replace(/(&#x)(\w{4});/gi, function ($0) {
+    return String.fromCharCode(parseInt(encodeURIComponent($0).replace(/(%26%23x)(\w{4})(%3B)/g, '$2'), 16))
+  })
+  return str
+}
+var wrap = function (render) {
+  return function () {
+    return render.apply(this, arguments)
+      .replace('<code class="', '<code class="hljs ')
+      .replace('<code>', '<code class="hljs">')
+  }
+}
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': resolve('src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [resolve('src'), resolve('test')],
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test')]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.md$/,
+        loader: 'vue-markdown-loader',
+        options: {
+          // markdown-it config
+          // preset: 'default',
+          breaks: true,
+          preprocess: function (MarkdownIt, source) {
+            MarkdownIt.renderer.rules.table_open = function () {
+              return '<table class="table">'
+            }
+            MarkdownIt.renderer.rules.fence = wrap(MarkdownIt.renderer.rules.fence)
+            return source
+          },
+          use: [
+            [require('markdown-it-anchor'), {
+              level: 2,
+              slugify: slugify,
+              permalink: true,
+              permalinkBefore: true
+            }],
+            [require('markdown-it-container'), 'demo', {
+              validate: function (params) {
+                return params.trim().match(/^demo\s*(.*)$/)
+              },
+
+              render: function (tokens, idx) {
+                var m = tokens[idx].info.trim().match(/^demo\s*(.*)$/)
+                if (tokens[idx].nesting === 1) {
+                  var description = (m && m.length > 1) ? m[1] : ''
+                  var content = tokens[idx + 1].content
+                  var html = convert(striptags.strip(content, ['script', 'style'])).replace(/(<[^>]*)=""(?=.*>)/g, '$1')
+                  var script = striptags.fetch(content, 'script')
+                  var style = striptags.fetch(content, 'style')
+                  var jsfiddle = { html: html, script: script, style: style }
+                  var descriptionHTML = description
+                    ? md.render(description)
+                    : ''
+
+                  jsfiddle = md.utils.escapeHtml(JSON.stringify(jsfiddle))
+
+                  return `<demo-block class="demo-box" :jsfiddle="${jsfiddle}">
+                            <div class="source" slot="source">${html}</div>
+                            ${descriptionHTML}
+                            <div class="highlight" slot="highlight">`
+                }
+                return '</div></demo-block>\n'
+              }
+            }],
+            [require('markdown-it-container'), 'tip']
+          ]
+        }
+      }
+    ]
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,127 @@
+<style lang="scss">
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+#app {
+  height: 100%;
+}
+
+body {
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB', 'Microsoft YaHei', SimSun, sans-serif;
+  overflow: auto;
+  font-weight: 400;
+  -webkit-font-smoothing: antialiased;
+}
+
+a {
+  color: #4078c0;
+  text-decoration: none;
+}
+
+code {
+  background-color: #f9fafc;
+  padding: 0 4px;
+  border: 1px solid #eaeefb;
+  border-radius: 4px;
+}
+
+button,
+input,
+select,
+textarea {
+  font-family: inherit;
+  font-size: inherit;
+  line-height: inherit;
+  color: inherit;
+}
+
+.hljs {
+  line-height: 1.8;
+  font-family: Menlo, Monaco, Consolas, Courier, monospace;
+  font-size: 12px;
+  padding: 18px 24px;
+  background-color: #f9fafc;
+  border: solid 1px #eaeefb;
+  margin-bottom: 25px;
+  border-radius: 4px;
+  -webkit-font-smoothing: auto;
+}
+
+.main-cnt {
+  margin-top: -80px;
+  padding: 80px 0 120px;
+  box-sizing: border-box;
+  min-height: 100%;
+}
+
+.container,
+.page-container {
+  width: 1140px;
+  padding: 0 30px;
+  margin: 0 auto;
+}
+
+.demo {
+  margin: 20px 0;
+}
+
+@media (max-width: 1140px) {
+  .container,
+  .page-container {
+    width: 100%;
+  }
+}
+
+@media (max-width: 768px) {
+  .container,
+  .page-container {
+    padding: 0 20px;
+  }
+}
+
+.header-anchor {
+  opacity: 0;
+}
+
+table.table {
+  border-collapse: collapse;
+  width: 100%;
+  background-color: #fff;
+  color: #5e6d82;
+  font-size: 14px;
+  margin-bottom: 45px;
+  line-height: 1.5em;
+  th {
+    border-bottom: 1px solid #eaeefb;
+    padding: 10px;
+    max-width: 250px;
+    text-align: left;
+    border-top: 1px solid #eaeefb;
+    background-color: #eff2f7;
+    white-space: nowrap;
+  }
+  td {
+    border-bottom: 1px solid #eaeefb;
+    padding: 10px;
+    max-width: 250px;
+  }
+}
+</style>
+
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import 'highlight.js/styles/color-brewer.css'
+
+export default {
+  name: 'app'
+}
+</script>
